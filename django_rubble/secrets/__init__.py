62e0bf740668c95bd0f0f6651193b936896b68ca
Karl Wooster <karl.wooster@alleima.com>
2024-10-17 10:59:54 -0700

    build(cli): don't expose "env" if typer is not installed (cli extra not installed)

--- django_rubble/secrets/__init__.py
+++ django_rubble/secrets/__init__.py
@@ -1,4 +1,10 @@
-from . import env
+from importlib.util import find_spec
+
 from .infisical import Secrets
 
-__all__ = ["Secrets", "env"]
+__all__ = ["Secrets"]
+
+if find_spec("typer") is not None:
+    from . import env
+
+    __all__ += ["env"]
